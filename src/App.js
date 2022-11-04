bcf5cede5c7d27f81eb511ae688e47b5df7f55df
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-11-04 14:13:27 +0700

    props album

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.css';
+import AlbumFeature from './features/Album';
 
 function App() {
+  // const name = 'Long';
+  // const age = 18;
+  // const isMale = true;
+  // const student = {
+  //   name: 'Suleng',
+  // };
+  // const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Su Leng</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          Xin chao {name}: {age} - {isMale ? 'Male' : 'Female'}
+        </p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Famale</p>}
+
+        {isMale && (
+          <div>
+            <p>Male1</p>
+            <p>Male2</p>
+            <p>Male3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male3</p>
+            <p>Male4</p>
+            <p>Male5</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male6</p>
+            <p>Male7</p>
+            <p>Male8</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map((color) => (
+            <li key={color} style={{ color }}>
+              {color}
+            </li>
+          ))}
+        </ul>
+      </header> */}
+      {/* <TodoFeature /> */}
+      <AlbumFeature />
     </div>
   );
 }
 
 export default App;
+// ôn lại bài 16
